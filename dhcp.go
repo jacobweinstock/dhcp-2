70db17aee623ddaa28139425f703f19380cf9be4
Jacob Weinstock <jakobweinstock@gmail.com>
2022-01-08 12:37:51 -0700

    dont use coredhcp, doesnt work on darwin
    Signed-off-by: Jacob Weinstock <jakobweinstock@gmail.com>

--- dhcp.go
+++ dhcp.go
@@ -2,82 +2,122 @@ package dhcp
 
 import (
 	"context"
-	"fmt"
 	"net"
-	"reflect"
 
-	"github.com/coredhcp/coredhcp/config"
-	"github.com/coredhcp/coredhcp/plugins"
-	"github.com/coredhcp/coredhcp/server"
 	"github.com/go-logr/logr"
-	"github.com/imdario/mergo"
+	"github.com/insomniacslk/dhcp/dhcpv4"
+	"github.com/insomniacslk/dhcp/dhcpv4/server4"
+	"github.com/jacobweinstock/dhcp/backend/tink"
+	"golang.org/x/sync/errgroup"
+	"inet.af/netaddr"
 )
 
+type BackendReader interface {
+	// Read from a backend and return DHCP headers and options based on the mac address.
+	Read(context.Context, net.HardwareAddr) ([]dhcpv4.Modifier, error)
+}
+
 type Server struct {
-	Log  logr.Logger
-	Addr net.UDPAddr
+	ctx        context.Context
+	Log        logr.Logger
+	ListenAddr netaddr.IPPort
+	Backend    BackendReader
 }
 
-type Handler interface {
-	Name(context.Context) string
-	Args(context.Context) []string
-	Handle(context.Context) *plugins.Plugin
-}
-
-func (s *Server) ListenAndServe(ctx context.Context, h Handler) error {
-	defaults := Server{
-		Log:  logr.Discard(),
-		Addr: net.UDPAddr{Port: 67},
+// New returns a proxy DHCP server for the Handler.
+func (s *Server) ListenAndServe(ctx context.Context) error {
+	// for broadcast traffic we need to listen on all IPs
+	conn := &net.UDPAddr{
+		IP:   net.ParseIP("0.0.0.0"),
+		Port: s.ListenAddr.UDPAddr().Port,
 	}
+	s.Backend = &tink.Conn{}
 
-	err := mergo.Merge(s, defaults, mergo.WithTransformers(s))
+	// server4.NewServer() will isolate listening to the specific interface.
+	srv, err := server4.NewServer(getInterfaceByIP(s.ListenAddr.IP().String()), conn, s.handleFunc)
 	if err != nil {
 		return err
 	}
 
-	l := s.Log
-	l.Info("debugging", "name", h.Name(ctx))
-	if err := plugins.RegisterPlugin(h.Handle(ctx)); err != nil {
-		return fmt.Errorf("error registering plugin: %w", err)
-	}
-
-	cfg := &config.Config{
-		Server4: &config.ServerConfig{
-			Addresses: []net.UDPAddr{
-				s.Addr,
-			},
-			Plugins: []config.PluginConfig{
-				{Name: h.Name(ctx), Args: h.Args(ctx)},
-			},
-		},
-	}
-	// start server
-	srv, err := server.Start(cfg)
-	if err != nil {
-		return err
-	}
+	g, ctx := errgroup.WithContext(ctx)
+	g.Go(func() error {
+		return srv.Serve()
+	})
 
 	<-ctx.Done()
-	srv.Close()
-	l.Info("shutting down")
-	return nil
+
+	return srv.Close()
 }
 
-// Transformer for merging the logr.Logger struct.
-// Can be extended for other types by adding cases.
-func (*Server) Transformer(typ reflect.Type) func(dst, src reflect.Value) error {
-	switch typ {
-	case reflect.TypeOf(logr.Logger{}):
-		return func(dst, src reflect.Value) error {
-			if dst.CanSet() {
-				getSink := dst.MethodByName("GetSink")
-				result := getSink.Call(nil)
-				if result[0].IsNil() {
-					dst.Set(src)
+func (s *Server) handleFunc(conn net.PacketConn, peer net.Addr, m *dhcpv4.DHCPv4) {
+	var reply *dhcpv4.DHCPv4
+	switch mt := m.MessageType(); mt {
+	case dhcpv4.MessageTypeDiscover:
+		reply = s.handleDiscover(s.ctx, m)
+	case dhcpv4.MessageTypeRequest:
+		reply = s.handleRequest(s.ctx, m)
+	case dhcpv4.MessageTypeRelease:
+		s.handleRelease(s.ctx, m)
+		return
+	default:
+		s.Log.Info("received unknown message type", "type", mt)
+	}
+	s.Log.Info(reply.Summary())
+	if _, err := conn.WriteTo(reply.ToBytes(), peer); err != nil {
+		s.Log.Error(err, "failed to send DHCP")
+	}
+}
+
+func (s *Server) handleDiscover(ctx context.Context, m *dhcpv4.DHCPv4) *dhcpv4.DHCPv4 {
+	s.Log.Info("received discover, sending offer")
+	mods, err := s.Backend.Read(ctx, m.ClientHWAddr)
+	if err != nil {
+		return nil
+	}
+	mods = append(mods, dhcpv4.WithMessageType(dhcpv4.MessageTypeOffer))
+	reply, err := dhcpv4.NewReplyFromRequest(m, mods...)
+	if err != nil {
+		return nil
+	}
+	return reply
+}
+
+func (s *Server) handleRequest(ctx context.Context, m *dhcpv4.DHCPv4) *dhcpv4.DHCPv4 {
+	s.Log.Info("received request, sending ack")
+	mods, err := s.Backend.Read(ctx, m.ClientHWAddr)
+	if err != nil {
+		return nil
+	}
+	mods = append(mods, dhcpv4.WithMessageType(dhcpv4.MessageTypeAck))
+	reply, err := dhcpv4.NewReplyFromRequest(m, mods...)
+	if err != nil {
+		return nil
+	}
+	return reply
+}
+
+func (s *Server) handleRelease(ctx context.Context, m *dhcpv4.DHCPv4) {
+	s.Log.Info("received release, no response required")
+}
+
+// getInterfaceByIP returns the interface with the given IP address or an empty string.
+func getInterfaceByIP(ip string) string {
+	ifaces, err := net.Interfaces()
+	if err != nil {
+		return ""
+	}
+	for _, iface := range ifaces {
+		addrs, err := iface.Addrs()
+		if err != nil {
+			continue
+		}
+		for _, addr := range addrs {
+			if ipnet, ok := addr.(*net.IPNet); ok {
+				if ipnet.IP.String() == ip {
+					return iface.Name
 				}
 			}
-			return nil
 		}
 	}
-	return nil
+	return ""
 }
--- go.mod
+++ go.mod
@@ -3,39 +3,20 @@ module github.com/jacobweinstock/dhcp
 go 1.17
 
 require (
-	github.com/bombsimon/logrusr/v2 v2.0.1
-	github.com/chappjc/logrus-prefix v0.0.0-20180227015900-3a1d64819adb
-	github.com/coredhcp/coredhcp v0.0.0-20211129110046-dcca4ae19a45
 	github.com/go-logr/logr v1.2.2
 	github.com/go-logr/stdr v1.2.2
-	github.com/imdario/mergo v0.3.12
 	github.com/insomniacslk/dhcp v0.0.0-20211214070828-5297eed8f489
-	github.com/sirupsen/logrus v1.8.1
+	golang.org/x/sync v0.0.0-20201020160332-67f06af15bc9
+	inet.af/netaddr v0.0.0-20211027220019-c74959edd3b6
 )
 
 require (
-	github.com/fsnotify/fsnotify v1.4.9 // indirect
-	github.com/google/gopacket v1.1.19 // indirect
-	github.com/hashicorp/hcl v1.0.0 // indirect
-	github.com/magiconair/properties v1.8.4 // indirect
-	github.com/mattn/go-colorable v0.1.8 // indirect
-	github.com/mattn/go-isatty v0.0.12 // indirect
-	github.com/mgutz/ansi v0.0.0-20200706080929-d51e80ef957d // indirect
-	github.com/mitchellh/mapstructure v1.4.1 // indirect
-	github.com/pelletier/go-toml v1.8.1 // indirect
-	github.com/rifflock/lfshook v0.0.0-20180920164130-b9218ef580f5 // indirect
-	github.com/spf13/afero v1.5.1 // indirect
-	github.com/spf13/cast v1.3.1 // indirect
-	github.com/spf13/jwalterweatherman v1.1.0 // indirect
-	github.com/spf13/pflag v1.0.6-0.20201009195203-85dd5c8bc61c // indirect
-	github.com/spf13/viper v1.7.1 // indirect
-	github.com/subosito/gotenv v1.2.0 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/stretchr/testify v1.7.0 // indirect
 	github.com/u-root/uio v0.0.0-20210528114334-82958018845c // indirect
-	golang.org/x/crypto v0.0.0-20201221181555-eec23a3978ad // indirect
+	go4.org/intern v0.0.0-20211027215823-ae77deb06f29 // indirect
+	go4.org/unsafe/assume-no-moving-gc v0.0.0-20211027215541-db492cf91b37 // indirect
 	golang.org/x/net v0.0.0-20210119194325-5f4716e94777 // indirect
 	golang.org/x/sys v0.0.0-20210608053332-aa57babbf139 // indirect
-	golang.org/x/term v0.0.0-20201210144234-2321bbc49cbf // indirect
-	golang.org/x/text v0.3.5 // indirect
-	gopkg.in/ini.v1 v1.62.0 // indirect
-	gopkg.in/yaml.v2 v2.4.0 // indirect
+	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
 )
