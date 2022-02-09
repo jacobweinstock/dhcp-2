6d66eeb43912f257ea349251dbe35d9761b5f6cf
Jacob Weinstock <jakobweinstock@gmail.com>
2022-02-09 08:06:46 -0700

    update readme, etc
    Signed-off-by: Jacob Weinstock <jakobweinstock@gmail.com>

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # dhcp
-DHCP backed by Tink server
+
+DHCP is a dhcp server backed by Tink server. All IP addresses are served as DHCP reservations. There are no leases.
+
+## Definitions
+
+**DHCP Reservation:**
+A fixed IP address that is reserved for a specific client.
+
+**DHCP Lease:**
+An IP address, that can potentially change, that is assigned to a client by the DHCP server.
+The IP is typically pulled from a pool or subnet of available IP addresses.
--- backend/tink/tink.go
+++ backend/tink/tink.go
@@ -9,31 +9,37 @@ import (
 )
 
 type Conn struct {
-	IP      netaddr.IPPort
-	CertURL string
+	NetbootDisabled bool
+	ServerIP        netaddr.IPPort
+	ServerCertURL   string
 }
 
 func (c *Conn) Read(ctx context.Context, mac net.HardwareAddr) ([]dhcpv4.Modifier, error) {
-	withNetboot := func(d *dhcpv4.DHCPv4) {
-		d.BootFileName = "undionly.kpxe"
-		d.ServerIPAddr = net.IPv4(192, 168, 2, 225)
+	var withNetboot func(d *dhcpv4.DHCPv4)
+	if !c.NetbootDisabled {
+		pxe := dhcpv4.Options{
+			// PXE Boot Server Discovery Control - bypass, just boot from filename.
+			6: []byte{8}, // or []byte{8}
+		}
+		withNetboot = func(d *dhcpv4.DHCPv4) {
+			d.BootFileName = "undionly.kpxe"
+			d.ServerIPAddr = net.IPv4(192, 168, 1, 225)
+			d.UpdateOption(dhcpv4.OptGeneric(dhcpv4.OptionVendorSpecificInformation, pxe.ToBytes()))
+		}
+
 	}
-	pxe := dhcpv4.Options{
-		// PXE Boot Server Discovery Control - bypass, just boot from filename.
-		6: []byte{8}, // or []byte{8}
-	}
+
 	mods := []dhcpv4.Modifier{
 		dhcpv4.WithDNS(net.IP{8, 8, 8, 8}),
 		dhcpv4.WithNetmask(net.IPMask{255, 255, 255, 0}),
-		dhcpv4.WithRouter(net.IP{192, 168, 2, 1}),
+		dhcpv4.WithRouter(net.IP{192, 168, 1, 1}),
 		dhcpv4.WithLeaseTime(3600),
-		dhcpv4.WithServerIP(net.IP{192, 168, 2, 225}),
-		dhcpv4.WithYourIP(net.IP{192, 168, 2, 152}),
-		dhcpv4.WithGeneric(dhcpv4.OptionServerIdentifier, net.IP{192, 168, 2, 225}),
+		dhcpv4.WithServerIP(net.IP{192, 168, 1, 225}),
+		dhcpv4.WithYourIP(net.IP{192, 168, 1, 152}),
+		dhcpv4.WithGeneric(dhcpv4.OptionServerIdentifier, net.IP{192, 168, 1, 225}),
 
 		// netboot options
 		withNetboot,
-		dhcpv4.WithGeneric(dhcpv4.OptionVendorSpecificInformation, pxe.ToBytes()),
 	}
 	return mods, nil
 }
